b47bcac269f08437a966c36c45c9cb12106cb26d
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-04 03:40:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
-def prime?(num)
-  if num == 
-    return true 
-  else
+def prime?(*num)
+  nums.each do |num|
+    i = 2 
+    while i < num 
+  if num % i == 0 
     return false 
+  else
+    return true 
   end 
+  i += 1
 end
   # Add  code here!
